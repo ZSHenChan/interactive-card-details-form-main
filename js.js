5a9ee57e3e2ed46267775160b878e199fd0f7cce
Zhen <zishenchan@gmail.com>
2023-08-18 18:33:01 +0800

    display user's input
    to-do:
    check user input
    web UI

--- js.js
+++ js.js
@@ -1,24 +1,34 @@
 const form = document.getElementById("form");
+const errorMsg = document.getElementById("error-msg");
+const cardNumInput = document.getElementById("card-num-input");
+
+function validCardNum(){
+    if (cardNumInput.match(/[0-9]{4}[\s][0-9]{4}[\s][0-9]{4}[\s][0-9]{4}$/)){
+        errorMsg.style.display="none";
+        cardNumInput.style.borderColor="hsl(270, 3%, 87%)";
+        return true;
+    }
+    else{
+        errorMsg.style.display="flex";
+        cardNumInput.style.borderColor="red";
+        return false;
+    }
+}
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
-    const cardholderName = document.getElementById("cardholder-name");
-    // localStorage.setItem("userName",cardholderName);
-    const cardNumber = document.getElementById("card-number");
-    // localStorage.setItem("userCardNum",cardNumber);
-    const expMonth = document.getElementById("exp-month");
-    const expYear = document.getElementById("exp-year");
-    // localStorage.setItem("exp-date",)
-    const cvc = document.getElementById("cvc");
 
-    console.log(cardholderName);
-    const cardHolderNameDisplay = document.getElementById("cardholder-name-display");
-    const cardNumberDisplay = document.getElementById("card-number-display");
-    const expDateDisplay = document.getElementById("exp-date-display");
-    const cvcDisplay = document.getElementById("cvc-display");
+    if (validCardNum()){
+        const cardholderName = document.getElementById("cardholder-name");
+        localStorage.setItem("userName",cardholderName.value);
+        const cardNumber = document.getElementById("card-number");
+        localStorage.setItem("userCardNum",cardNumber.value);
+        const expMonth = document.getElementById("exp-month");
+        const expYear = document.getElementById("exp-year");
+        localStorage.setItem("exp-date",expMonth.value+"/"+expYear.value);
+        const cvc = document.getElementById("cvc");
+        localStorage.setItem("cvc",cvc.value)
 
-    cardHolderNameDisplay.innerHTML = cardholderNam.value;
-    cardNumberDisplay.innerHTML = cardNumber.value;
-    expDateDisplay.innerHTML = expMonth.value+'/'+expYear.value;
-    cvcDisplay.innerHTML = cvc.value;
+        window.location.href="complete.html";
+    }
 })
